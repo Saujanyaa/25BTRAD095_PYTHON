b5175e8768b0b1df8279b2d1d83e21cf0cf391dd
Saujanyaa <130643243+Saujanyaa@users.noreply.github.com>
2026-08-15 11:26:23 +0530

    Add files via upload

---
+++ 25BTRAD095_LAB EXPERIMENTS/Unit_1_Lab_Activities_2&3.py
@@ -0,0 +1,8 @@
+#Excuting a simple Python Program
+print("Hello World")
+print()
+
+#Prgram using variables, operators and input/output
+a = int(input("Enter first number :"))
+b = int(input("Enter second number : "))
+print(f"Sum of {a} and {b} is {a+b}")
